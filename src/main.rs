515042e4294e42812fc435e06e0eaacccab2614a
Nathan Whitehead <nwhitehe@gmail.com>
2025-05-24 18:07:23 -0600

    Show pads. Try with diodes.

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ struct MyApp {
 
 impl Default for MyApp {
     fn default() -> Self {
-        let bytes = include_bytes!("./BJT.json");
+        let bytes = include_bytes!("./Diode.json");
         let parsed: Value = serde_json::from_slice(bytes).unwrap();
         Self {
             lib: parsed,
@@ -66,10 +66,12 @@ impl MyApp {
     }
 }
 
+/// Add heading text UI element (big font)
 fn heading(text: &str) -> egui::Label {
     egui::Label::new(egui::RichText::new(text).font(egui::FontId::proportional(20.0)))
 }
 
+/// Given JSON library chunk of a component, extract part that is DRAW if found
 fn find_draw(v: &Value) -> Option<&Value> {
     for i in 0..v.as_array().unwrap().len() {
         if v[i][0] == serde_json::Value::String("DRAW".into()) {
@@ -79,6 +81,7 @@ fn find_draw(v: &Value) -> Option<&Value> {
     return None;
 }
 
+/// Given a JSON value, try to parse as a f32 number
 fn parse_number(v: &Value) -> Option<f32> {
     match v.as_number() {
         Some(n) => Some(n.as_f64().unwrap() as f32),
@@ -86,42 +89,14 @@ fn parse_number(v: &Value) -> Option<f32> {
     }
 }
 
+/// Apply Affine2 transformation to Pos2 coordinate
 fn apply_affine2(v: &Pos2, a: &Affine2) -> Pos2 {
     let p = a.transform_point2(Vec2::new(v.x, v.y));
-    let sx = a.transform_vector2(Vec2::new(1.0, 0.0));
     return Pos2::new(p.x, p.y);
 }
 
-fn draw_to_shape(v: &Value, transform: &Affine2, color: Color32) -> Shape {
-    let mut shapes = vec![];
-    let n = v.as_array().unwrap().len();
-    for i in 0..n {
-        if let Some(s) = drawline_to_shape(&v[i], &transform, color) {
-            shapes.push(s);
-        }
-    }
-    return Shape::Vec(shapes);
-}
-
-fn draw_to_padpos(v: &Value, transform: &Affine2) -> Vec<Pos2> {
-    let mut res = vec![];
-    let n = v.as_array().unwrap().len();
-    for i in 0..n {
-        let color = Color32::WHITE;
-        let vi = &v[i];
-        let a = vi.as_array().unwrap();
-        let tag = &a[0];
-        let ts = tag.as_str().unwrap();
-        if ts == "X" {
-            let (x, y);
-            x = parse_number(&a[3]).unwrap();
-            y = parse_number(&a[4]).unwrap();
-            res.push(apply_affine2(&Pos2::new(x, y), transform));
-        }
-    }
-    return res;
-}
-
+/// Helper function for draw_to_shape
+// Turns one line of DRAW section into a Shape
 fn drawline_to_shape(v: &Value, transform: &Affine2, color: Color32) -> Option<Shape> {
     let a = v.as_array().unwrap();
     let tag = &a[0];
@@ -183,6 +158,38 @@ fn drawline_to_shape(v: &Value, transform: &Affine2, color: Color32) -> Option<S
     return None;
 }
 
+/// Given DRAW JSON value, turn section into single Shape for drawing
+fn draw_to_shape(v: &Value, transform: &Affine2, color: Color32) -> Shape {
+    let mut shapes = vec![];
+    let n = v.as_array().unwrap().len();
+    for i in 0..n {
+        if let Some(s) = drawline_to_shape(&v[i], &transform, color) {
+            shapes.push(s);
+        }
+    }
+    return Shape::Vec(shapes);
+}
+
+/// Find pad positions of symbol, given DRAW section
+fn draw_to_padpos(v: &Value, transform: &Affine2) -> Vec<Pos2> {
+    let mut res = vec![];
+    let n = v.as_array().unwrap().len();
+    for i in 0..n {
+        let color = Color32::WHITE;
+        let vi = &v[i];
+        let a = vi.as_array().unwrap();
+        let tag = &a[0];
+        let ts = tag.as_str().unwrap();
+        if ts == "X" {
+            let (x, y);
+            x = parse_number(&a[3]).unwrap();
+            y = parse_number(&a[4]).unwrap();
+            res.push(apply_affine2(&Pos2::new(x, y), transform));
+        }
+    }
+    return res;
+}
+
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let nth = &self.lib[self.n][1];
@@ -211,9 +218,12 @@ impl eframe::App for MyApp {
             let painter = ui.painter();
             let transform = Affine2::from_scale_angle_translation(Vec2::new(1.0, 1.0), 3.14159*0.0, Vec2::new(300.0, 250.0));
             let color = Color32::WHITE;
+            let lead_color = Color32::YELLOW;
             painter.add(draw_to_shape(&draw, &transform, color));
             let leads = draw_to_padpos(&draw, &transform);
-            println!("Padpos = {:?}", leads);
+            for lead in leads {
+                painter.add(Shape::circle_filled(lead, 5.0, lead_color));
+            }
         });
     }
 }
