b95ffdcc464c8ca201758674b13bf7752f18846d
Nathan Whitehead <nwhitehe@gmail.com>
2025-08-13 10:26:43 -0600

    Convert to uniform ground handling, one more var doesn't matter

--- src/sim.py
+++ src/sim.py
@@ -30,10 +30,10 @@ class CurrentSource:
         return ComponentType.CURRENT_SOURCE
 
 def count_num_voltages(netlist):
-    n = 0
+    n = -1
     for (component, pos, neg) in netlist:
         n = max(n, pos, neg)
-    return n
+    return n + 1
 
 def count_num_voltage_sources(netlist):
     m = 0
@@ -59,35 +59,26 @@ def generate_mna(netlist):
             # Passive components go into upperleft n x n part of a
             c = component.conductance()
             # On diagonal is sum of conductances
-            if pos > 0:
-                a[pos - 1, pos - 1] += c
-            if neg > 0:
-                a[neg - 1, neg - 1] += c
+            a[pos, pos] += c
+            a[neg, neg] += c
             # Off diagonal is sum of negated conductances, symmetric
-            # Off diagonal is only relevant if no ground connection
-            if pos > 0 and neg > 0:
-                a[pos - 1, neg - 1] -= c
-                a[neg - 1, pos -1 ] -= c
+            a[pos, neg] -= c
+            a[neg, pos] -= c
         elif t == ComponentType.VOLTAGE_SOURCE:
             # Voltage sources go into upper right and lower left sections
             # Positive 1 for positive connection side, -1 for negative side
-            # Connections to ground don't have an entry
-            if pos > 0:
-                a[pos - 1, n + mi] = 1
-                a[n + mi, pos - 1] = 1
-            if neg > 0:
-                a[neg - 1, n + mi] = -1
-                a[n + mi, neg - 1] = -1
+            a[pos, n + mi] = 1
+            a[n + mi, pos] = 1
+            a[neg, n + mi] = -1
+            a[n + mi, neg] = -1
             # Also record voltage in RHS
             b[n + mi] = component.v
             mi += 1
         elif t == ComponentType.CURRENT_SOURCE:
             # Current source point from negative to positive
             # So more like current out, current in
-            if pos > 0:
-                b[pos - 1] += component.i
-            if neg > 0:
-                b[neg - 1] -= component.i
+            b[pos] += component.i
+            b[neg] -= component.i
     return (a, b)
 
 def main():
@@ -111,13 +102,17 @@ def main():
         (vss, 2, 1),
         (vextra, 3, 0),
     ]
-    assert count_num_voltages(netlist) == 3
+    assert count_num_voltages(netlist) == 4
     assert count_num_voltage_sources(netlist) == 2
     a, b = generate_mna(netlist)
-    assert np.allclose(a, np.array([[0.5, 0, 0, -1, 0], [0, 0.375, -0.25, 1, 0], [0, -0.25, 0.25, 0, 1], [-1, 1, 0, 0, 0], [0, 0, 1, 0, 0]]))
-    assert np.allclose(b, np.array([0, 0, 0, 32, 20]))
+    assert np.allclose(a, np.array([[0.625, -0.5, -0.125, 0, 0, -1], [-0.5, 0.5, 0, 0, -1, 0], [-0.125, 0, 0.375, -0.25, 1, 0],
+                                    [0, 0, -0.25, 0.25, 0, 1], [0, -1, 1, 0, 0, 0], [-1, 0, 0, 1, 0, 0]]))
+    assert np.allclose(b, np.array([0, 0, 0, 0, 32, 20]))
     x = np.linalg.solve(a, b)
-    assert np.allclose(x, np.array([-8, 24, 20, -4, 1]))
+    # Check relative voltages to ground
+    assert np.allclose(x[1:4] - x[0], [-8, 24, 20])
+    # Check currents
+    assert np.allclose(x[4:], [-4, 1])
 
     # Case 2
     netlist = [
@@ -128,10 +123,13 @@ def main():
         (iss, 1, 0),
     ]
     a, b = generate_mna(netlist)
-    assert np.allclose(a, np.array([[0.75, -0.25, 1], [-0.25, 0.375, -1], [1, -1, 0]]))
-    assert np.allclose(b, np.array([0.25, 0, 32]))
+    assert np.allclose(a, np.array([[0.625, -0.5, -0.125, 0], [-0.5, 0.75, -0.25, 1], [-0.125, -0.25, 0.375, -1], [0, 1, -1, 0]]))
+    assert np.allclose(b, np.array([-0.25, 0.25, 0, 32]))
     x = np.linalg.solve(a, b)
-    assert np.allclose(x, np.array([6.8, -25.2, -11.15]))
+    # Check relative voltages to ground
+    assert np.allclose(x[1:3] - x[0], [6.8, -25.2])
+    # Check current
+    assert np.allclose(x[3:], -11.15)
 
 
 if __name__ == '__main__':
