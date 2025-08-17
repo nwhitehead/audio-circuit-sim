acd21e7bf70d9efa26d8d7d81d74b892eecff95a
Nathan Whitehead <nwhitehe@gmail.com>
2025-08-17 13:13:46 -0600

    Printing binops.

--- src/symbolic.py
+++ src/symbolic.py
@@ -3,6 +3,7 @@ Symbolic equation solving
 
 """
 
+from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
 import numpy as np
@@ -24,12 +25,55 @@ class Variable:
             fresh_id += 1
         self.name = name
         existing_variables.add(name)
+    def __str__(self):
+        return self.name
+
+@dataclass
+class VarExpr:
+    v: Variable
+    def __str__(self):
+        return f'{self.v}'
+
+class BinOp(Enum):
+    PLUS = 1
+    MINUS = 2
+    TIMES = 3
+    DIVIDE = 4
+    def __str__(self):
+        match self:
+            case BinOp.PLUS:
+                return '+'
+            case BinOp.MINUS:
+                return '-'
+            case BinOp.TIMES:
+                return '*'
+            case BinOp.DIVIDE:
+                return '/'
+            case _:
+                raise ValueError()
+
+@dataclass
+class BinExpr:
+    op: str
+    left: Expr
+    right: Expr
+    def __str__(self):
+        return f'({self.left} {self.op} {self.right})'
+
+Expr = VarExpr | BinExpr
 
 def main():
     x = Variable()
     y = Variable()
     z = Variable()
-    print(x, y, z)
+    assert(f'{x} {y} {z}' == 'v v0 v1')
+    assert(x == x)
+    assert(x != y)
+    v = VarExpr(x)
+    print(v)
+    v0 = BinExpr(BinOp.PLUS, VarExpr(x), VarExpr(y))
+    v1 = BinExpr(BinOp.DIVIDE, v0, VarExpr(x))
+    print(v1)
 
 if __name__ == '__main__':
     main()
