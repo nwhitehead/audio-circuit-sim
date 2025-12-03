76447b9415554790aa4a75cc2d4c801d81141902
Nathan Whitehead <nwhitehe@gmail.com>
2025-12-03 12:58:41 -0800

    Start translating halite code to rust directly.

--- src/sim.rs
+++ src/sim.rs
@@ -1,17 +1,19 @@
 
-pub struct Variable {
-    /// Variable that we are solving for in the circuit simulation.
-    name: String,
-}
+/// Show pivot details in LU factorization
+const VERBOSE_LU: bool = true;
 
-pub enum BinOp {
-    Add, Subtract, Multiply, Divide,
-}
+/// Stepsize for linearization of non-linear components
+const G_MIN: f64 = 1e-12;
 
-pub enum Expression {
-    V(Variable),
-    Bin(BinOp, )
-}
+/// Voltage tolerance for iterative solver
+const V_TOLERANCE: f64 = 5e-5;
+
+/// Thermal voltage for diode and transistor model
+const V_THERMAL: f64 = 0.0258;
+
+/// Maximum number of iterations in main netlist loop
+const MAX_ITER: u32 = 200;
+
 
 fn main() {
     println!("Hello from sim.rs");
