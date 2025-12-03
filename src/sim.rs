bc4d1d8baddc4ec3a1d5e19fb74fb39b412f0ef7
Nathan Whitehead <nwhitehe@gmail.com>
2025-12-03 13:15:42 -0800

    Working on translation, at MNACell

--- src/sim.rs
+++ src/sim.rs
@@ -9,11 +9,62 @@ const G_MIN: f64 = 1e-12;
 const V_TOLERANCE: f64 = 5e-5;
 
 /// Thermal voltage for diode and transistor model
-const V_THERMAL: f64 = 0.0258;
+const V_THERMAL: f64 = 0.026;
 
 /// Maximum number of iterations in main netlist loop
 const MAX_ITER: u32 = 200;
 
+//
+// General overview
+// ----------------
+//
+// Circuits are built from nodes and Components, where nodes are
+// simply positive integers (with 0 designating ground).
+//
+// Every Component has one or more pins connecting to the circuit
+// nodes as well as zero or more internal nets.
+//
+// While we map pins directly to nets here, the separation would
+// be useful if the solver implemented stuff like net-reordering.
+//
+// MNACell represents a single entry in the solution matrix,
+// where we store constants and time-step dependent constants
+// separately, plus collect pointers to dynamic variables.
+//
+// We track enough information here that we only need to stamp once.
+//
+// Lifetime is needed to make sure dynamic references are live when used.
+//
+struct MNACell<'a> {
+    // simple values (eg. resistor conductance)
+    g: f64,
+    // time-scaled values (eg. capacitor conductance)
+    g_timed: f64,
+    // references to dynamic variables
+    g_dyn: Vec<&'a f64>,
+    // LU value and pre-LU cache value
+    lu: f64,
+    pre_lu: f64,
+    // Debug info
+    txt: String,
+}
+
+impl Default for MNACell<'_> {
+    fn default() -> Self { MNACell {
+        g: 0.0,
+        g_timed: 0.0,
+        g_dyn: vec![],
+        lu: 0.0,
+        pre_lu: 0.0,
+        txt: String::new(),
+    } }
+}
+
+impl <'a> MNACell<'a> {
+    fn clear() {
+
+    }
+}
 
 fn main() {
     println!("Hello from sim.rs");
